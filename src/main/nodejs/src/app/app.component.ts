11802c69f1ab881823eeff9f40efea3f9586b309
Fabien Crespel <fabien@crespel.net>
2019-10-30 00:06:49 +0100

    Improve home page design and use fixed navbar

--- src/main/nodejs/src/app/app.component.ts
+++ src/main/nodejs/src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  currentRoute: string;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event: RouterEvent) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => this.currentRoute = event.urlAfterRedirects);
+  }
+
+}
